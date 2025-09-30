0ceceeae5d72ddae09b8e8e145f5194f6cbd8592
Tomi <admin@mcitomi.hu>
2025-09-30 15:12:06 +0200

    magprog szept 30

---
+++ magprog1/20250930fenyok/20250930fenyok/Program.cs
@@ -0,0 +1,63 @@
+﻿// https://xdepot.uni-eszterhazy.hu/index.php/s/i66uTfLh5XjkSqc?path=%2FMagasszint%C5%B1%20programoz%C3%A1si%20nyelvek%201%2FMinta%20ZH#pdfviewer
+namespace _20250930fenyok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.Write("Mennyi fát termelnek 1nap alatt?: ");
+            int faPerNap;
+
+            while (!int.TryParse(Console.ReadLine(), out faPerNap) || faPerNap < 35 || faPerNap > 55)
+            {
+                Console.Write("Hibás bevitel! Mennyi fát termelnek 1nap alatt?: ");
+            }
+
+            int[] fak = new int[faPerNap * 14];
+
+            int[] legkisebbfa = new int[2]; // nap, fa cm
+
+            int atlagMagassagTemp = 0;
+
+            int legnagyobbOsszertekuNap = 0;
+            int legnagyobbOsszertekuNapTermelese = 0;
+
+            int osszeretek = 0;
+
+            for (int i = 0; i < fak.Length; i++)
+            {
+                fak[i] = random.Next(150, 300 + 1);
+
+                atlagMagassagTemp += fak[i];
+                if((i + 1) % faPerNap == 0)
+                {
+                    int aktualisNapTermelese = ((atlagMagassagTemp / faPerNap) / 10) * 500;
+                    if (legnagyobbOsszertekuNapTermelese < aktualisNapTermelese)
+                    {
+                        legnagyobbOsszertekuNap = i / faPerNap + 1;
+                        legnagyobbOsszertekuNapTermelese = aktualisNapTermelese;
+                    }
+
+                    Console.WriteLine($"Az {i / faPerNap + 1}. nap avg magassága {(float)(atlagMagassagTemp / faPerNap) / 100} méter.");
+                    atlagMagassagTemp = 0;
+                }
+
+                if (fak[i] < legkisebbfa[1] || legkisebbfa[1] == 0)
+                {
+                    legkisebbfa[1] = fak[i];
+                    legkisebbfa[0] = i / faPerNap + 1;
+                }
+
+                osszeretek += (fak[i] / 10) * 500;
+            }
+
+            Console.WriteLine($"\nA legkisebb fát a {legkisebbfa[0]}. napon teremelték ki és {(float)legkisebbfa[1] / 100} méter volt.");
+            Console.WriteLine($"\nHa minden fát kitermelnénk {osszeretek:N0} Ft bevételre számíthatnánk.");
+            Console.WriteLine($"\nA legnagyobb összértékű kitermelés a {legnagyobbOsszertekuNap}. napon volt ami {legnagyobbOsszertekuNapTermelese:N0} Ft.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ magprog1/20250930peldazh/20250930peldazh/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _20250930peldazh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pizzaAr = 1350;
+            int feltetekSzama = 0;
+
+            string[] kat200 = { "sonka", "kukorica", "gomba" };
+            string[] kat250 = { "kolbász", "ananász", "jalapenho" };
+            string[] kat300 = { "kagyló", "articsóka", "oliva" };
+
+            Console.WriteLine("Üdvözli önt a MixAPizza pizzéria!");
+
+            string feltet = "";
+
+            while (feltetekSzama < 5 && feltet != "-")
+            {
+                Console.Write("Adjon meg egy feltétet: ");
+                feltet = Console.ReadLine();
+
+                foreach (var item in kat200)
+                {
+                    if(item == feltet)
+                    {
+                        pizzaAr += 200;
+                        feltetekSzama++;
+                        break;
+                    }
+                }
+
+                foreach (var item in kat250)
+                {
+                    if (item == feltet)
+                    {
+                        pizzaAr += 250;
+                        feltetekSzama++;
+                        break;
+                    }
+                }
+
+                foreach (var item in kat300)
+                {
+                    if (item == feltet)
+                    {
+                        pizzaAr += 300;
+                        feltetekSzama++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"A pizzára {feltetekSzama} db feltét került, a pizza {pizzaAr} Ft lesz");
+        }
+    }
+}
