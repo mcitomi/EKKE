686782a8d0e95237977ad71915039793f6654fad
mcitomi <berdotamas5@gmail.com>
2025-10-28 16:01:30 +0100

    birocs binaris cs

---
+++ birocs/cs/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace cs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dec = 0;
+        string bin = "10111";
+        int help = 1;
+
+        for (int i = bin.Length - 1; i >= 0; i--) {
+            dec += (bin[i]-'0') * help;
+            help*=2;
+        }
+        System.Console.WriteLine(dec);
+    }
+}
