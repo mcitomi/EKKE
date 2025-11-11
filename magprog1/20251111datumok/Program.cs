7c7409047eaf230245b1546462a4402cdf3df0bb
mcitomi <berdotamas5@gmail.com>
2025-11-11 15:12:47 +0100

    datumok fuggvenyek KGS

---
+++ magprog1/20251111datumok/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace _20251111datumok;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "15.11.2025 14:30";
+        DateTime d = DateTime.ParseExact(s, "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
+
+        // 1. elozo_napi: napi dátumból (Now) vonjunk ki 2 hónapot és 1 napot
+        DateTime elozo_napi = DateTime.Now.AddMonths(-2).AddDays(-1);
+
+        // 2. napi dátumból (Today) adjunk hozzá 1évet és 20percet
+        System.Console.WriteLine(DateTime.Today.AddYears(1).AddMinutes(20));
+
+        // 3. kozeljovo: tároljuk el a megfelelő tipusú változóban 2044 év 10 hónap 5 napját
+        DateOnly kozeljovo = new DateOnly(2044, 10, 5);
+
+        // 4. negyvenes_evek: tároljuk el megfelelő típusban a "1944-12-10" szövegnek megfelelő dátumot
+        DateOnly negyvenes_evek = DateOnly.Parse("1944-12-10");
+
+        // 5. írjuk ki a közeljövőből az évet
+        System.Console.WriteLine(kozeljovo.Year);
+
+        // 6. írjuk ki a közeljövőt "2044 év és 10 hónap meg 5 nap" formában
+        System.Console.WriteLine($"{kozeljovo.Year} év és {kozeljovo.Month} hónap meg {kozeljovo.Day} nap");
+
+        // 7. írjuk ki a negyvenes_evek-et "pár nap múlva 1945 mert már a hónap 10. napja van 12 hónapban 1944 évben"
+        System.Console.WriteLine($"pár nap múlva {negyvenes_evek.AddYears(1).Year} mert már a hónap {negyvenes_evek.Day}. napja van {negyvenes_evek.Month} hónapban {negyvenes_evek.Year} évben");
+        
+        // 8. írjuk ki az elozo_napi-ból "nap.hónap.év -> óra:perc:másodperc" formában
+        System.Console.WriteLine(elozo_napi.ToString("dd.MM.yyyy -> HH:mm:ss"));
+    }
+}
---
+++ magprog1/20251111fuggvenyek/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _20251111fuggvenyek;
+
+internal class B
+{
+    public int Szam1 { get; set; }
+    public B(int szam)
+    {
+        this.Szam1 = szam;
+    }
+}
+internal class A
+{
+    public B InternalClass { get; set; }
+}
+class Program
+{
+    static void Test(ref int val)
+    {
+        val = 20;
+    }
+    static void Main(string[] args)
+    {
+        // int szam = 10;
+        // Test(ref szam);
+
+        A elso = new A();
+        elso.InternalClass = new B(33);
+
+        System.Console.WriteLine(elso.InternalClass.Szam1);
+
+        B tmp = elso.InternalClass;
+        tmp.Szam1 -= 2;
+
+        System.Console.WriteLine(elso.InternalClass.Szam1);
+    }
+}
